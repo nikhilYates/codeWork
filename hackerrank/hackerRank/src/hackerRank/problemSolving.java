f6d6d127de4dcf5c4743dd0be24b72abda4a25c1
yates-nikhil <nikhilyy@my.yorku.ca>
2023-01-29 19:27:22 -0500

    Update problemSolving.java

--- hackerrank/hackerRank/src/hackerRank/problemSolving.java
+++ hackerrank/hackerRank/src/hackerRank/problemSolving.java
@@ -1,4 +1,5 @@
 package hackerRank;
+import java.util.*;
 
 public class problemSolving {
 	
@@ -27,5 +28,32 @@ public class problemSolving {
 	        }
 		
 	}
+	
+	public static List<Integer> breakingRecords(List<Integer> scores) {
+	    // Write your code here
+	    int highScore = scores.get(0);
+	    int minScore = scores.get(0);
+	    int highRecords = 0;
+	    int minRecords = 0;
+	    
+	    for(int i : scores) {
+	        if(i > highScore) {
+	            highScore = i;
+	            highRecords++;
+	        }
+	        if (i < minScore) {
+	            minScore = i;
+	            minRecords++;
+	        }
+	    }
+	    
+	    List<Integer> records = new ArrayList<Integer>();
+	    records.add(highRecords);
+	    records.add(minRecords);
+	    
+	    return records;
+
+	    }  
+	
 
 }
