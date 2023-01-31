ea4cd8a0004f16a4b23a1ca2fa5b52ec3595fe10
yates-nikhil <nikhilyy@my.yorku.ca>
2023-01-31 16:23:12 -0500

    quick changes

--- hackerrank/hackerRank/src/hackerRank/problemSolving.java
+++ hackerrank/hackerRank/src/hackerRank/problemSolving.java
@@ -6,7 +6,7 @@ public class problemSolving {
 	
 	public static void main(String[] args) {
 		
-		
+		whereDoItFit();
 		numProgression(2, 4, 3, 1);
 	}
 	
@@ -66,6 +66,35 @@ public class problemSolving {
 	    System.out.println(bigFact);
 
 	    }
+	
+	public static void whereDoItFit() {
+		
+		System.out.println("Enter an integer: ");
+		
+		Scanner sc = new Scanner(System.in);
+	        int t=sc.nextInt();
+
+	        for(int i=0;i<t;i++)
+	        {
+
+	            try
+	            {
+	                long x=sc.nextLong();
+	                System.out.println(x+" can be fitted in:");
+	                if(x>=-128 && x<=127)System.out.println("* byte");
+	                //Complete the code
+	                if(x>=-32768 && x<=32767)System.out.println("* short");
+	                if(x>= Math.pow(-2, 32) && x <= Math.pow(2, 32) - 1) System.out.println("* int");
+	                if(x>= Math.pow(-2, 64) && x <= Math.pow(2, 64) - 1) System.out.println("* long");
+	            }
+	            catch(Exception e)
+	            {
+	                System.out.println(sc.next()+" can't be fitted anywhere.");
+	            }
+	            
+
+	        }
+	}
 	
 
 }
