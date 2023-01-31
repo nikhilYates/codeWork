ae0f061619c3294ebc3b49a47bab66addd312515
yates-nikhil <nikhilyy@my.yorku.ca>
2023-01-31 17:36:32 -0500

    revArr

--- hackerrank/hackerRank/src/hackerRank/problemSolving.java
+++ hackerrank/hackerRank/src/hackerRank/problemSolving.java
@@ -96,5 +96,15 @@ public class problemSolving {
 	        }
 	}
 	
+	public static List<Integer> reverseArray(List<Integer> a) {
+	    // Write your code here
+	        List<Integer> revArr = new ArrayList<Integer>();
+	        for(int i = a.size() - 1; i >= 0; i--) {
+	            revArr.add(a.get(i));
+	        }
+	        
+	        return revArr;
+	    }
+	
 
 }
